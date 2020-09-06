3dce4d5011b7637a0ef598efff1c50b90ad744b7
Siddhant <mamtajain81@gmail.com>
2020-09-06 14:31:13 +0530

    Updates

---
+++ precache-manifest.5b1ecec5fd5958c410633641a7af1994.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b791646d27b48b83121558f81adc0d",
+    "url": "/rEACT-LIVE-WEBSITE/index.html"
+  },
+  {
+    "revision": "30387f2a2c6260ef5a0a",
+    "url": "/rEACT-LIVE-WEBSITE/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "265d8ba3fd3a7d34523a",
+    "url": "/rEACT-LIVE-WEBSITE/static/js/2.b81abd36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rEACT-LIVE-WEBSITE/static/js/2.b81abd36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30387f2a2c6260ef5a0a",
+    "url": "/rEACT-LIVE-WEBSITE/static/js/main.c6924254.chunk.js"
+  },
+  {
+    "revision": "b5d2c2ea531a42b46d51",
+    "url": "/rEACT-LIVE-WEBSITE/static/js/runtime-main.e4b188b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rEACT-LIVE-WEBSITE/static/media/logo.5d5d9eef.svg"
+  }
+]);
